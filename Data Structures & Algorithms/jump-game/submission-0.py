7789ab1bec6b9824ce7f583d594f7fbcedaaf98e
Rushikesh282006 <28rushikeshjagtap@gmail.com>
2026-08-08 21:20:11 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        n = len(nums)
+        max_reach = 0
+
+        for i in range(n):
+
+            if i>max_reach:
+                return False
+
+            max_reach = max(max_reach,i+nums[i])    
+
+            if max_reach >= n:
+                return True
+            
+        return True
